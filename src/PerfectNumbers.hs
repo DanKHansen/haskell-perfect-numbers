b670e49df95ddd46bd992b7ec2eb529bdbf37c72
Dan Kjeldstrøm Hansen <dankh@live.dk>
2025-07-17 14:43:49 +0200

    solution

--- src/PerfectNumbers.hs
+++ src/PerfectNumbers.hs
@@ -1,6 +1,26 @@
-module PerfectNumbers (classify, Classification(..)) where
+module PerfectNumbers (classify, Classification (..)) where
+
+import Data.Foldable (foldl')
 
 data Classification = Deficient | Perfect | Abundant deriving (Eq, Show)
 
 classify :: Int -> Maybe Classification
-classify = error "You need to implement this function."
+classify n
+  | n <= 0 = Nothing
+  | n == 1 = Just Deficient
+  | otherwise =
+      let sumDivisors =
+            foldl'
+              ( \s i ->
+                  if n `mod` i == 0
+                    then s + i + if i /= 1 && i /= n `div` i then n `div` i else 0
+                    else s
+              )
+              0
+              [1 .. floor (sqrt (fromIntegral n :: Double))]
+       in if sumDivisors == n
+            then Just Perfect
+            else
+              if sumDivisors > n
+                then Just Abundant
+                else Just Deficient
