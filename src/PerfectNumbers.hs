e70f1d56709293fe98215e2bcd64e8f211c194bb
Dan Kjeldstrøm Hansen <dankh@live.dk>
2025-07-17 14:46:20 +0200

    refactor

--- src/PerfectNumbers.hs
+++ src/PerfectNumbers.hs
@@ -9,18 +9,8 @@ classify n
   | n <= 0 = Nothing
   | n == 1 = Just Deficient
   | otherwise =
-      let sumDivisors =
-            foldl'
-              ( \s i ->
-                  if n `mod` i == 0
-                    then s + i + if i /= 1 && i /= n `div` i then n `div` i else 0
-                    else s
-              )
-              0
-              [1 .. floor (sqrt (fromIntegral n :: Double))]
-       in if sumDivisors == n
-            then Just Perfect
-            else
-              if sumDivisors > n
-                then Just Abundant
-                else Just Deficient
+      let sumDivisors = foldl' (\s i -> s + if n `mod` i == 0 then i + if i /= 1 && i /= n `div` i then n `div` i else 0 else 0) 0 [1 .. floor (sqrt (fromIntegral n :: Double))]
+       in Just $ case compare sumDivisors n of
+            EQ -> Perfect
+            GT -> Abundant
+            LT -> Deficient
