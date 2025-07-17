40d7aab04d1cf117eb7b178166cc00793c77d796
Dan Kjeldstrøm Hansen <dankh@live.dk>
2025-07-17 14:55:17 +0200

    refactor

--- src/PerfectNumbers.hs
+++ src/PerfectNumbers.hs
@@ -1,16 +1,12 @@
 module PerfectNumbers (classify, Classification (..)) where
 
-import Data.Foldable (foldl')
-
 data Classification = Deficient | Perfect | Abundant deriving (Eq, Show)
 
 classify :: Int -> Maybe Classification
 classify n
   | n <= 0 = Nothing
-  | n == 1 = Just Deficient
-  | otherwise =
-      let sumDivisors = foldl' (\s i -> s + if n `mod` i == 0 then i + if i /= 1 && i /= n `div` i then n `div` i else 0 else 0) 0 [1 .. floor (sqrt (fromIntegral n :: Double))]
-       in Just $ case compare sumDivisors n of
-            EQ -> Perfect
-            GT -> Abundant
-            LT -> Deficient
+  | aliq < n = Just Deficient
+  | aliq == n = Just Perfect
+  | otherwise = Just Abundant
+  where
+    aliq = sum $ filter (\i -> mod n i == 0) [1 .. n - 1]
