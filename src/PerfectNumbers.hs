a039a95dbd94c7150857abcf6d820465bd5e7400
Dan Kjeldstrøm Hansen <dankh@live.dk>
2025-07-17 15:14:37 +0200

    refactor

--- src/PerfectNumbers.hs
+++ src/PerfectNumbers.hs
@@ -5,8 +5,10 @@ data Classification = Deficient | Perfect | Abundant deriving (Eq, Show)
 classify :: Int -> Maybe Classification
 classify n
   | n <= 0 = Nothing
-  | aliq < n = Just Deficient
-  | aliq == n = Just Perfect
-  | otherwise = Just Abundant
+  | otherwise = Just $ case compare aliq n of
+      LT -> Deficient
+      EQ -> Perfect
+      GT -> Abundant
   where
-    aliq = sum $ filter (\i -> mod n i == 0) [1 .. n - 1]
+    aliq = sum $ filter p [1 .. div n 2]
+    p i = mod n i == 0
